6e4682cd323847beb3fd694d695d0f4ff9336e9f
mchaki@DOMBLANQUI.LOCAL <chakiamine25@gmail.com>
2024-12-03 17:58:36 +0100

    fe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ActiviteComponent} from './component/activite/activite.component';
+import {AdherentComponent} from './component/adherent/adherent.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ActiviteComponent, AdherentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'DMZ-IF';
+  title = '93 moove';
+  date = new Date();
 }
---
+++ src/app/models/activite.model.ts
@@ -0,0 +1,4 @@
+export interface Activite {
+  id: number;
+  types: string;
+}
